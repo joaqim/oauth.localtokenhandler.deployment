2a56f36d9fb2c8428b87a00ffd5205a8693118c5
gary <gary@MACSTATION.local>
2022-03-17 16:57:14 +0000

    Updates

--- build.sh
+++ build.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-##############################################################################
-# A script to deploy Token Handler resources to a local Docker compose network
-##############################################################################
+####################################################################################
+# A script to deploy local token handler resources to a local Docker compose network
+####################################################################################
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 #
-# Download SSL certificates
+# Download SSL certificates used by the OAuth Agent and OAuth Proxy
 #
-rm -rf certs
-git clone https://github.com/gary-archer/oauth.developmentcertificates ./certs
+rm -rf tmp
+git clone https://github.com/gary-archer/oauth.developmentcertificates ./tmp
 if [ $? -ne 0 ]; then
     echo 'Problem encountered downloading API certificates'
     exit 1
 fi
-cd certs
-cd ..
+rm -rf certs
+mv ./tmp/authsamples-dev ./certs
 
 #
 # Download the Curity OAuth proxy plugin
@@ -29,18 +29,30 @@ if [ $? -ne 0 ]; then
 fi
 
 #
-# Download the main OAuth Agent API
+# Download the OAuth Agent API
 #
-rm -rf oauth-agent-api
-git clone https://github.com/gary-archer/oauth.tokenhandler.docker oauth-agent-api
+rm -rf oauth-agent
+git clone https://github.com/gary-archer/oauth.tokenhandler.docker oauth-agent
 if [ $? -ne 0 ]; then
     echo 'Problem encountered downloading the Token Handler API'
     exit 1
 fi
-cd oauth-agent-api
+cd oauth-agent
 
 #
-# Install API dependencies
+# When the OAuth Proxy calls an API on the host computer, this ensures that SSL trust works
+# If also running a proxy tool such as Charles on the host, the proxy root CA may cause SSL trust problems
+# To resolve this, set an environment variable that includes both the below CA and the proxy root CA
+#
+echo $TOKEN_HANDLER_CA_CERTS
+if [[ -z "$TOKEN_HANDLER_CA_CERTS" ]]; then
+  cp ../tmp/mycompany.ca.pem ./trusted.ca.pem
+else
+  cp $TOKEN_HANDLER_CA_CERTS ./trusted.ca.pem
+fi
+
+#
+# Install OAuth Agent dependencies
 #
 rm -rf node_modules
 npm install
@@ -50,7 +62,7 @@ if [ $? -ne 0 ]; then
 fi
 
 #
-# Build the token handler API's code
+# Build the OAuth Agent's API's code
 #
 npm run buildRelease
 if [ $? -ne 0 ]; then
@@ -59,21 +71,10 @@ if [ $? -ne 0 ]; then
 fi
 
 #
-# If the token handler calls a local API, this ensures that SSL trust works
-# If also running a proxy tool such as Charles on the host, the proxy root CA may cause SSL trust problems
-# To resolve this, set an environment variable that includes both the below CA and the proxy root CA
+# Build the OAuth Agent's Docker container
 #
-if [[ -z "$TOKEN_HANDLER_CA_CERTS" ]]; then
-  cp ../certs/mycompany.ca.pem ./trusted.ca.pem
-else
-  cp $TOKEN_HANDLER_CA_CERTS ./trusted.ca.pem
-fi
-
-#
-# Build the API's Docker container
-#
-docker build -f ./Dockerfile -t tokenhandler:v1 .
+docker build -f ./Dockerfile -t oauthagent:v1 .
 if [ $? -ne 0 ]; then
-  echo "Problem encountered building the Token Handler API Docker container"
+  echo "Problem encountered building the OAuth Agent Docker container"
   exit 1
 fi
--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
 
-###############################################################
-# A script to run the Token Handler in a local Docker container
-###############################################################
+####################################################################################
+# A script to deploy local token handler resources to a local Docker compose network
+####################################################################################
 
 #
 # Set the current folder if this script is called from another script
 #
 cd "$(dirname "${BASH_SOURCE[0]}")"
-BUSINESS_API_URL='https://api.mycompany.com:445/api'
-
-#
-# Do some string manipulation to update kong.yml with the runtime value for the Business API URL
-#
-ESCAPED_URL=$(echo $BUSINESS_API_URL | sed "s/\//\\\\\//g")
-KONG_YML_DATA=$(cat ./apigateway/kong.template.yml)
-KONG_YML_DATA=$(sed "s/BUSINESS_API_URL/$ESCAPED_URL/g" <<< "$KONG_YML_DATA")
-echo "$KONG_YML_DATA" > ./apigateway/kong.yml
 
 #
 # Run the Docker compose network
